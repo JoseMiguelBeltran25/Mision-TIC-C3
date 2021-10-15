cd3a1f1f0979b5fbda08f8415966fec2fceeb4ad
brayanovalle16 <brayanovalle2@outlook.com>
2021-10-14 22:48:06 -0500

    Control Acceso

--- ProyectoCiclo3.App.Frontend/Pages/Estaciones/Create.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Estaciones/Create.cshtml.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProyectoCiclo3.App.Persistencia.AppRepositorios;
 using ProyectoCiclo3.App.Dominio;
- 
+
 namespace ProyectoCiclo3.App.Frontend.Pages
-{
+{ [Authorize]
     public class FormEstacionModel : PageModel
     {
  
